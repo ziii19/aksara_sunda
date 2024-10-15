701926ae55abdb09e29d2affc0fac911816b1671
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-10-15 15:06:08 +0700

    test conflict

--- example/aksara_sunda_example.dart
+++ example/aksara_sunda_example.dart
@@ -1,10 +1,11 @@
-import 'package:aksara_sunda/aksara_sunda.dart';
-import 'package:aksara_sunda/aksara_sunda_2.dart';
+import '../lib/aksara_sunda.dart';
+import '../lib/sunda_3.dart';
 
 void main(List<String> args) {
   var teks = AksaraSunda().sundaToLatin('ᮠᮜ᮪ ᮓᮥᮔ᮪');
   SundaTranslator translator = SundaTranslator();
   String hasil = translator.latin2Sunda("Sunda ngaran abdi 123");
+  String teks1 = 'ini perubahan 1';
 
   print(hasil);
 
